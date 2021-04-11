2e8f76dcd98a25700984aea18f2d9674d72b1bfa
Ankit-Sharma-8110 <80452049+Ankit-Sharma-8110@users.noreply.github.com>
2021-04-11 11:54:02 +0530

    Update greetings.js

--- assignment/javascript/greetings.js
+++ assignment/javascript/greetings.js
@@ -3,25 +3,25 @@ keyLength = document.querySelectorAll(".greetings").length;
 console.log(keyLength);
 h1tag.style.color = "green";        
             
-function greetings() {
+let greetings = () => {
     //I'M UNABLE TO SET THE KEY. PLEASE HELP!!!
     for (let i = 0; i < keyLength; i++) {
         
         switch (i) {
             case 0:
-                h1tag.innerText = document.querySelectorAll(".greetings")[0].innerHTML;
+                h1tag.innerText = document.querySelectorAll(".greetings")[i].innerHTML;
                 break;
         
             case 1:
-                h1tag.innerText = document.querySelectorAll(".greetings")[1].innerHTML;
+                h1tag.innerText = document.querySelectorAll(".greetings")[i].innerHTML;
                 break;
         
             case 2:
-                h1tag.innerText = document.querySelectorAll(".greetings")[2].innerHTML;
+                h1tag.innerText = document.querySelectorAll(".greetings")[i].innerHTML;
                 break;
         
             case 3:
-                h1tag.innerText = document.querySelectorAll(".greetings")[3].innerHTML;
+                h1tag.innerText = document.querySelectorAll(".greetings")[i].innerHTML;
                 break;
         }
     }
